38dfb551633f79a3e72f657ae164bda240a3fa84
Lars Thise Pedersen <ltpe@techcollege.dk>
2025-05-21 15:09:48 +0200

    Lavet en Test case der altid fejler. Dette for at sikre at der altid bliver sendt en mail tilbage til ejeren af et Repository, når der er udført PUSH => CI/CD på et givet Repository. Det er så kun, hvis der er fejl i andre Test Cases også, at man som bruger skal reagere på dette.

---
+++ GitHub_Projekt1/MakeOneTestCaseWithErrorToAllowEMailNotificationcs.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace GitHub_Projekt1
+{
+    public class MakeOneTestCaseWithErrorToAllowEMailNotificationcs
+    {
+        [Fact]
+        public void MakeOnMakeOneTestCaseWithErrorToAllowEMailNotificationcs()
+        {
+            // Denne test fejler med vilje, så CI altid rapporterer mindst én fejl.
+            // Dermed får man altid sendt en mail tilbage, når
+            // man har lavet en PUSH => CI/CD af sit projekt. 
+            // Hvis dette er den ENESTE fejl, er alt OK!
+            Assert.True(false, "Denne test fejler altid med vilje. Se README for forklaring. Testen er lavet så, der altid bliver sendt en mail tilbage efter PUSH => CI/CD");
+        }
+    }
+}
